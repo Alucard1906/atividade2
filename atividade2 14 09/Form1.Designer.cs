1af3455aa6d00813d5a2a6f16f33fe428230ac18
DIMITRIGABRIELLIMADA <DIMITRIGABRIELLIMADA@MININT-Q33OIM1>
2023-09-14 11:38:30 -0300

    Adicionar arquivos de projeto.

---
+++ atividade2 14 09/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace atividade2_14_09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txb1 = new System.Windows.Forms.TextBox();
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.txb2 = new System.Windows.Forms.TextBox();
+            this.txb3 = new System.Windows.Forms.TextBox();
+            this.txb4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um Texto :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estilo DropdownList";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Posicao na Lista:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Texto Selecionado:";
+            // 
+            // txb1
+            // 
+            this.txb1.Location = new System.Drawing.Point(105, 16);
+            this.txb1.Name = "txb1";
+            this.txb1.Size = new System.Drawing.Size(430, 20);
+            this.txb1.TabIndex = 4;
+            // 
+            // cb1
+            // 
+            this.cb1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Location = new System.Drawing.Point(12, 78);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(523, 21);
+            this.cb1.TabIndex = 5;
+            this.cb1.SelectedIndexChanged += new System.EventHandler(this.cb1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Total de Itens:";
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(460, 197);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(75, 23);
+            this.bt1.TabIndex = 7;
+            this.bt1.Text = "Adicionar";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(460, 226);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(75, 23);
+            this.bt2.TabIndex = 8;
+            this.bt2.Text = "Eliminar";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.Location = new System.Drawing.Point(460, 255);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(75, 66);
+            this.bt3.TabIndex = 9;
+            this.bt3.Text = "Limpar toda a lista";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.Location = new System.Drawing.Point(460, 327);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(75, 23);
+            this.bt4.TabIndex = 10;
+            this.bt4.Text = "Limpar";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.Location = new System.Drawing.Point(460, 356);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(75, 23);
+            this.bt5.TabIndex = 11;
+            this.bt5.Text = "Sair";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // txb2
+            // 
+            this.txb2.Location = new System.Drawing.Point(117, 125);
+            this.txb2.Name = "txb2";
+            this.txb2.Size = new System.Drawing.Size(100, 20);
+            this.txb2.TabIndex = 12;
+            this.txb2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txb3
+            // 
+            this.txb3.Location = new System.Drawing.Point(117, 152);
+            this.txb3.Name = "txb3";
+            this.txb3.Size = new System.Drawing.Size(307, 20);
+            this.txb3.TabIndex = 13;
+            this.txb3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txb4
+            // 
+            this.txb4.Location = new System.Drawing.Point(117, 183);
+            this.txb4.Name = "txb4";
+            this.txb4.Size = new System.Drawing.Size(100, 20);
+            this.txb4.TabIndex = 14;
+            this.txb4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 400);
+            this.Controls.Add(this.txb4);
+            this.Controls.Add(this.txb3);
+            this.Controls.Add(this.txb2);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.txb1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txb1;
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.TextBox txb2;
+        private System.Windows.Forms.TextBox txb3;
+        private System.Windows.Forms.TextBox txb4;
+    }
+}
+
---
+++ atividade2 14 09/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atividade2_14_09
+{
+    public partial class Form1 : Form
+    {
+        string id;
+        int clear;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+      
+            
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+    
+            
+           
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            txb1.Clear();  
+            txb2.Clear();
+            txb3.Clear();
+            txb4.Clear();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            id = txb1.Text;
+            cb1.Items.Add(id);
+            txb1.Clear();
+            
+        }
+
+        private void cb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("pos:" + cb1.SelectedIndex);
+            txb2.Text = cb1.SelectedIndex.ToString();
+            txb3.Text = cb1.SelectedItem.ToString();
+            txb4.Text = cb1.Items.Count.ToString();
+
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            cb1.Items.RemoveAt(clear);
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            cb1.Items.Clear();
+        }
+    }
+}
